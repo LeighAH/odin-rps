e897e5ff51f66e4a01b87550b2a63609fe243cd1
Leigh <6755798+LeighAH@users.noreply.github.com>
2024-04-07 19:54:45 +0100

    Update the game loop to be way overengineered

--- script.js
+++ script.js
@@ -1,22 +1,84 @@
 class MoveType {
 
-    static TypeNames = ["Rock", "Paper", "Scissors"];
+    static NULL = {
+        value: "null",
+        number: 0,
+    }
+
+    static ROCK = {
+        value: "Rock",
+        number: 1,
+    }
+
+    static PAPER = {
+        value: "Paper",
+        number: 2,
+    }
+
+    static SCISSORS = {
+        value: "Scissors",
+        number: 3,
+    }
 
     constructor(type)
     {
         this.type = type;
-        this.name = MoveType.TypeNames[type-1];
+
+        if(this.type != MoveType.ROCK && this.type != MoveType.PAPER && this.type != MoveType.SCISSORS)
+        {
+            this.type = MoveType.NULL;
+        }
     }
 }
 
-Object.defineProperties(MoveType, {
-    ROCK: { enumerable: true, value: 1},
-    PAPER: { enumerable: true, value: 2},
-    SCISSORS: { enumerable: true, value: 3},
-});
-
 function getComputerMove() {
-    return new MoveType(getRandomInt(1,3));
+    let cpuChoice = getRandomInt(1,3);
+    let cpuMove;
+
+    switch(cpuChoice)
+    {
+        case 1: 
+            cpuMove = MoveType.ROCK;
+            break;
+        case 2:
+            cpuMove = MoveType.PAPER;
+            break;
+        case 3:
+            cpuMove = MoveType.SCISSORS;
+            break;
+    }
+
+    return new MoveType(cpuMove);
+}
+
+function getPlayerMove()
+{
+    let playerMove = MoveType.NULL;
+
+    while(playerMove == MoveType.NULL)
+    {
+        let playerChoice = prompt("Please choose a move. Rock, paper or scissors?");
+        playerChoice = sanitiseInput(playerChoice);
+    
+    
+        switch(playerChoice) {
+            case "Rock":
+                playerMove = MoveType.ROCK;
+                break;
+            case "Paper":
+                playerMove = MoveType.PAPER;
+                break;
+            case "Scissors":
+                playerMove = MoveType.SCISSORS;
+                break;
+            default:
+                playerMove = MoveType.NULL;
+                alert("Sorry, " + playerChoice + " is not a valid move.");
+    
+        }
+    }
+
+    return new MoveType(playerMove);
 }
 
 function getRandomInt(min, max) {
@@ -29,27 +91,62 @@ function getRandomInt(min, max) {
     return result
 }
 
-function evaluateWinner(playerMove, cpuMove)
+function sanitiseInput(input)
 {
-    if(playerMove.type === cpuMove.type) return "It's a tie!";
+    input = input.toLowerCase();
+    input = input.slice(0, 1).toUpperCase() + input.slice(1);
+    return input;
+}
+
+function playRound(playerMove, cpuMove)
+{
+    if(playerMove.type === cpuMove.type) return 0;
 
     if(playerMove.type == MoveType.ROCK && cpuMove.type == MoveType.SCISSORS || 
         playerMove.type == MoveType.PAPER && cpuMove.type == MoveType.ROCK ||
         playerMove.type == MoveType.SCISSORS && cpuMove.type == MoveType.PAPER) 
     {
-        return "Congratulations, you win!";
+        return 1;
     }
 
-    return "The computer wins, better luck next time.";
+    return 2;
 }
 
-let pm = new MoveType(parseInt(prompt("Please choose a move. 1 = Rock, 2 = Paper, 3 = Scissors")));
+function playGame(rounds) {
 
-if(!pm.type || pm.type < 1 || pm.type > 3) {
-    alert("Sorry, that was an invalid choice.");
+    let ties = 0;
+    let wins = 0;
+    let losses = 0;
+
+    for(i = 0; i < rounds; i++) {
+        pm = getPlayerMove();
+        cm = getComputerMove();
+        result = playRound(pm, cm);
+
+        if(result == 0) 
+        {
+            console.log("It's a tie!");
+            ties++;
+        }
+        if(result == 1)
+        {
+            console.log("Congratulations, you win!");
+            wins++;
+        }
+        if(result == 2) 
+        {
+            console.log("The computer wins, better luck next time.");
+            losses++;
+        }
+    }
+
+    console.log("What fun! Here are the results.")
+    console.log("Ties: " + ties);
+    console.log("You win: " + wins);
+    console.log("Computer wins: " + losses);
+    if(wins > losses) {
+        console.log("You won overall! Such skill and talent!");
+    }
 }
-else
-{
-    cm = getComputerMove();
-    alert("You chose " + pm.name + ". The computer chose " + cm.name + ". " + evaluateWinner(pm, cm));
-}
+
+playGame(5);
